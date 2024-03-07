5bc000d6e1998b8c50c2bbb7b62f69e3b6a87a1a
Agnes <Agnes@DESKTOP-UKFB1CL>
2024-03-07 14:01:42 +0100

    added image

--- webworks/src/Pages/Home.js
+++ webworks/src/Pages/Home.js
@@ -1,6 +1,10 @@
+import "./Home.css";
+
 const Home = () => {
   return(
-    <p>Hem</p>
+    <div>
+      <img src="https://cdn.pixabay.com/photo/2018/05/23/13/29/network-3424070_1280.jpg" alt="cool technology image"/>
+    </div>
   )
 }
 export default Home;
