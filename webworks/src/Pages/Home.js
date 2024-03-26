691feaa336b4517150969ca787549615909565ea
hebad <heba.derawi95@hotmail.com>
2024-03-26 16:35:06 +0100

    updated home to english

--- webworks/src/Pages/Home.js
+++ webworks/src/Pages/Home.js
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div className="welcome-container">
       <div className="welcome">
-        <h1>Välkommen till Webworks!</h1>
+        <h1>Welcome to Webworks!</h1>
         <img
           src="https://images.pexels.com/photos/325185/pexels-photo-325185.jpeg" // Använd src-attributet för att ladda bilden
           alt="Backround for welcome container"
